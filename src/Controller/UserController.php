a5e7e93c313a7c6e120815af56344f0942a0447d
Deniev <deniev@iMac-Deniev.local>
2021-08-24 10:07:36 +0200

    Profile birthday save bug fixed

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -86,8 +86,10 @@ class UserController extends CustomAbstractController
                 $user->getProfile()->setAvatar('avatar.jpg');
             }
 
-            if ($user->getProfile()->getBirthday() !== $this->getDoctrine()->getRepository(EmailAddress::class)->findOneBy(['address' => $user->getConfirmedEmail()])->getBirthday()) {
-                $this->getDoctrine()->getRepository(EmailAddress::class)->findOneBy(['address' => $user->getConfirmedEmail()])->setBirthday($user->getProfile()->getBirthday());
+            if ($user->getProfile()->getBirthday() && $user->getConfirmedEmail()) {
+                if ($user->getProfile()->getBirthday() !== $this->getDoctrine()->getRepository(EmailAddress::class)->findOneBy(['address' => $user->getConfirmedEmail()])->getBirthday()) {
+                    $this->getDoctrine()->getRepository(EmailAddress::class)->findOneBy(['address' => $user->getConfirmedEmail()])->setBirthday($user->getProfile()->getBirthday());
+                }
             }
 
             $this->getDoctrine()->getManager()->flush();
