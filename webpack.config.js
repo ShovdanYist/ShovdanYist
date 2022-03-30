126e95e6b5820de3b5688b7f41439bdd783dfa67
Deniev <deniev@iMac-Deniev.local>
2022-03-30 06:13:01 +0300

    Styles & scripts update

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('app2', './assets/js/app2.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
