9e0fa7dc9bffc5fbde4c4e891dfbe456114a67da
Deniev <deniev@iMac-Deniev.local>
2022-04-02 21:56:55 +0300

    Guest share error test

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -70,7 +70,7 @@ class PostController extends CustomAbstractController
             ->setPage($page)
         ;
 
-        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
+        if ($this->isGranted('ROLE_USER')) {
             $shareUsers = $this->getDoctrine()->getRepository(User::class)->findFollows(['user' => $this->user(), 'type' => 'following']);
         } else {
             $shareUsers = null;
